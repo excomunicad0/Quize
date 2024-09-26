340f3169a277c763b0667943c084819390a6c8e8
Анастасия Малютина <dima.urf@mail.ru>
2024-09-26 21:15:09 +0300

    feat/CSSregistration

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from 'react-router-dom';
-import './App.css';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { router } from './router/router';
--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './styles/index.css';
+import './styles/reset.css';
+import './styles/normalize.css';
+import './styles/variables.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
