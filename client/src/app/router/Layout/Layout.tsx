7c7d1c9c0a32c6b0c8f7656715ceae852872d811
Анастасия Малютина <dima.urf@mail.ru>
2024-09-26 18:29:45 +0300

    feat/createUser

--- client/src/app/router/Layout/Layout.tsx
+++ client/src/app/router/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 // import styles from './Layout.module.css';
 // import { Footer } from '@/widgets/Footer';
-// import { Navbar } from '@/widgets/Navbar';
+import { Navbar } from '@/widgets/Navbar';
 // import { Sidebar } from '@/widgets/Sidebar';
 import { Outlet } from 'react-router-dom';
 import { useAppDispatch } from '@/shared/hooks/reduxhooks';
@@ -18,6 +18,7 @@ const Layout: React.FC = () => {
 
   return (
     <>
+    <Navbar />
       <main >
         <Outlet />
       </main>
